6d85fad80cd3190c126dae81b665d3a6fa8ec6a5
Wallacy Rezende <wallacyrezende@gmail.com>
2021-04-22 14:49:44 -0300

    Desenvolvimento do Header e Player juntamente com a estilização em SASS

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+//toda vez que trocar de rota essa pagina é recalculada por inteira
+import '../styles/global.scss'
+
+import { Header } from '../components/Header'
+import { Player } from '../components/Player'
+
+import styles from '../styles/app.module.scss'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  )
+}
+
+export default MyApp
