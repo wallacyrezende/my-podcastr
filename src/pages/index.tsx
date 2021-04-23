017ded591782960d2cc29b6899fba11a26a3f1d5
Wallacy Rezende <wallacyrezende@gmail.com>
2021-04-22 22:28:31 -0300

    Implementando Static Side Generation com o NextJS e API fake com json-server

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,40 @@
-export default function Home() {
+/*SPA
+useEffect(() => {
+  fetch('http://localhost:3333/episodes')
+},[variables])
+*/
+
+/*SSR
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    }
+  }
+}
+*/
+
+//SSG
+export default function Home(props) {
   return (
-    <h1>Index</h1>
+    <>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
