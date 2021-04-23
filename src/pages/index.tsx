1788764daa1bc00244aa2fb55c32ebdbd75edb37
Wallacy Rezende <wallacyrezende@gmail.com>
2021-04-22 23:31:53 -0300

    Data fetch utilizando axios e tipando métodos e retornos da API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ useEffect(() => {
   fetch('http://localhost:3333/episodes')
 },[variables])
 */
-
 /*SSR
 export async function getServerSideProps() {
   const response = await fetch('http://localhost:3333/episodes');
@@ -16,24 +15,60 @@ export async function getServerSideProps() {
   }
 }
 */
-
 //SSG
-export default function Home(props) {
+import { GetStaticProps } from 'next';
+import { api } from '../services/api';
+import { format, parseISO } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+  duration: string;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
+}
+type HomeProps = {
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
   return (
     <>
-      <h1>Index</h1>
-      <p>{JSON.stringify(props.episodes)}</p>
+      <h1>Bem vindo ao seu Podcast</h1>
     </>
   )
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes');
-  const data = await response.json()
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('/episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  });
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+      duration: Number(episode.file.description),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    }
+  })
 
   return {
     props: {
-      episodes: data,
+      episodes,
     },
     revalidate: 60 * 60 * 8,
   }
